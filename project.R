f8c67dc43c488e200720299daf4f5d31a221583c
Cleveland, Brennan <Brennan.Cleveland@bestbuy.com>
2018-04-17 09:04:43 -0500

    Stuff

--- project.R
+++ project.R
@@ -6,20 +6,32 @@ count_words <- function(x) {
   length(strsplit(s,' ')[[1]])
 }
 
+# Sample n lines from a list of lines
+sample_lines(lines, n) {
+    lines[sample(length(lines), n)]
+}
+
+# Sample n lines from a file  
+sample_file <- function(fileName, n) {
+    conn <- file(fileName, "r")
+    lines <- readLines(conn)
+    close(conn)
+    sample_lines(lines, n)
+}
+
 #load 
-corpusPath <- "Coursera-SwiftKey/final/en_US"
+corpusPath <- "/Users/a1152802/Projects/Data/corpus"
 source <- DirSource(corpusPath)
 myCorpus <- Corpus(source)
 
+#clean
 myCorpus <- tm_map(myCorpus, stripWhitespace)
-myCorpus <- tm_map(myCorpus, content_transformer(tolower))
+myCorpus <- tm_map(myCorpus, removePunctuation)
+#myCorpus <- tm_map(myCorpus, content_transformer(tolower))
 
-# Get a sample of lines from one of the file
-doc <- myCorpus[[1]]$content
-lines <- sample(seq(1,length(doc)), 0.01 * length(doc))
-line <- do.call(paste, as.list(doc[lines]))
-
-ng <- ngram(line, n=3)
+#model
+doc <- do.call(paste, as.list(myCorpus))
+ng <- ngram(doc, n=3)
 p_table <- get.phrasetable(ng)
 
 predict_word <- function(x) {
@@ -27,3 +39,5 @@ predict_word <- function(x) {
   z <- p_table[grep(x, lapply(p_table$ngrams, substr, 0, nchar(x)), fixed=TRUE),]
   lapply(strsplit(z[1:3,"ngrams"], " ", fixed=TRUE), function(x) x[words + 1])
 }
+
+
